3c246ee648785471eb46b4a1d20088cf6ac82c1e
LordMustang <elionass58@gmail.com>
2019-05-15 15:05:37 +0200

    Fixed phone number regex

--- test/users.js
+++ test/users.js
@@ -10,6 +10,18 @@ const assert = chai.assert;
 //might be cool
 
 describe('Users', ()=> {
+    it('User should succesfully login', function (done) {
+        chai.request(server)
+            .post('/auth/login')
+            .set('Content-Type', 'application/json')
+            .send({
+                "email": "jsnow@avans.nl",
+                "password": "yoloswaggins"
+            })
+            .end(function (err, res) {
+                res.should.have.status(200);
+                done();
+            })
+    });
 
-    it('')
 })
