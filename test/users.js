5e747100b29043da98e445ac1d11f5c5c0d11587
Laerositus <jpc.stander@student.avans.nl>
2019-05-15 13:59:14 +0200

    Fixing test so we can go testing

---
+++ test/users.js
@@ -0,0 +1,14 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../index.js');
+
+chai.should();
+
+chai.use(chaiHttp);
+
+const assert = chai.assert;
+
+describe('Users', ()=> {
+
+    it('')
+})
